e11ffca7a0bc40cca0dd78e03c80fb02bebffe1d
Jake <jake.ale@outlook.com>
2020-09-23 14:19:25 -0700

    some rich text support

--- virtool_cli/taxid.py
+++ virtool_cli/taxid.py
@@ -2,10 +2,12 @@ import os
 import time
 import json
 import asyncio
+from rich.console import Console
+from rich.progress import track
 from Bio import Entrez
 from concurrent.futures.thread import ThreadPoolExecutor
 
-Entrez.email  = os.environ["NCBI_EMAIL"]
+Entrez.email = os.environ["NCBI_EMAIL"]
 API_KEY = os.environ["NCBI_API_KEY"]
 
 
@@ -16,12 +18,12 @@ async def run(src_path: str):
     paths = get_paths(src_path)
 
     coros = list()
-
-    for path in paths[:5]:
+    console = Console()
+    for path in track(paths, description="[green]Retrieving..."):
         name = get_name_from_path(path)
-        
+
         if name:
-            coro = loop.run_in_executor(executor, fetch_taxid, name)
+            coro = loop.run_in_executor(executor, fetch_taxid, name, console)
             coros.append(coro)
 
             await asyncio.sleep(0.3)
@@ -29,15 +31,15 @@ async def run(src_path: str):
             paths.remove(path)
 
     results = await asyncio.gather(*coros)
-    
-    
+
     names = []
     taxids = []
+
     # parse through results
     for name, taxid in results:
         names.append(name)
         taxids.append(taxid)
-    
+
     for name, taxid, path, in zip(names, taxids, paths):
         with open(os.path.join(path, "otu.json"), 'r+') as f:
             otu = json.load(f)
@@ -46,25 +48,23 @@ async def run(src_path: str):
                 otu["taxid"] = taxid
                 f.seek(0)
                 json.dump(otu, f, indent=4)
-    
+
     if not results:
         print("All OTUs are up to date")
+    console = Console()
+    console.print(f"Retrieved {len(taxids)} taxids for {len(names)} OTUs", style="green")
 
-    print(f"Retrieved {len(taxids)} taxids for {len(names)} OTUs")
-        
 
-
-def fetch_taxid(name):
+def fetch_taxid(name, console):
     handle = Entrez.esearch(db="taxonomy", term=name, api_key=API_KEY)
     record = Entrez.read(handle)
 
     try:
         taxid = record["IdList"][0]
-        print(f"Received: {name} {taxid}")
+        console.print(f"    {name} {taxid} ✔️", style="green")
     except IndexError:
-        print(f"Could not receive: {name}")
+        console.print(f"Could not retrieve: {name} ❌", style="red")
         taxid = None
-
 
     return name, taxid
 
@@ -77,8 +77,9 @@ def get_paths(src_path: str):
         if alpha == "meta.json":
             continue
 
-        otu_paths = [os.path.join(src_path, alpha, otu) for otu in os.listdir(os.path.join(src_path, alpha))]
-        
+        otu_paths = [os.path.join(src_path, alpha, otu)
+                     for otu in os.listdir(os.path.join(src_path, alpha))]
+
         for otu in otu_paths:
             paths.append(otu)
 
@@ -90,7 +91,8 @@ def get_name_from_path(path):
         otu = json.load(f)
 
         try:
-            if otu["taxid"]: return None
+            if otu["taxid"]:
+                return None
         except KeyError:
             return otu["name"]
 
