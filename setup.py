460ffffd63ecc5a4f4e57f76fc647d44d2a3f426
Jake <jake.ale@outlook.com>
2020-10-02 15:19:25 -0700

    Modify setup to find package

--- setup.py
+++ setup.py
@@ -2,16 +2,15 @@ import setuptools
 
 
 setuptools.setup(
-    name="virtool-cli",
-    packages=setuptools.find_packages(),
+    name="virtool_cli",
+    packages=setuptools.find_packages(exclude="tests"),
     version="0.0.1",
     description="a command line interface for working with Virtool data",
-    py_modules=["run"],
-    install_requires=['Click'],
+    install_requires=["click"],
     url="https://github.com/virtool/virtool-cli",
     entry_points={
-        'console.scripts': [
-            'virtool = run:cli',
+        "console_scripts": [
+            "virtool = virtool_cli.run:cli",
         ],
     }
 )
