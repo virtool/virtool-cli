88154359960fdc0f2a443b9e7a19cc40ee7cff74
Jake <jake.ale@outlook.com>
2020-10-05 15:51:54 -0700

    Update setup.py install requirements

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ import setuptools
 with open("README.md", "r") as readme:
     long_description = readme.read()
 
+with open("requirements.txt", "r") as f:
+    install_requires = f.read()
+
 setuptools.setup(
     name="virtool_cli",
     version="0.0.1",
@@ -13,7 +16,7 @@ setuptools.setup(
     python_requires=">=3.6",
     url="https://github.com/virtool/virtool-cli",
     packages=setuptools.find_packages(exclude="tests"),
-    install_requires=["click"],
+    install_requires=install_requires,
     entry_points={
         "console_scripts": [
             "virtool = virtool_cli.run:cli",
