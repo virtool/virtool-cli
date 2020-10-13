9298d6ad6f747ed3af1a0f1c7ed90702f89db85b
Jake Alexander <jake.ale@outlook.com>
2020-10-13 14:20:27 -0700

    Update helper functions and cache strategy

--- virtool_cli/isolate.py
+++ virtool_cli/isolate.py
@@ -3,9 +3,11 @@ import json
 import os
 from concurrent.futures.thread import ThreadPoolExecutor
 from random import choice
+
+import shutil
 from string import ascii_letters, ascii_lowercase, digits
 from typing import Union, Iterable
-
+from urllib.error import HTTPError
 import aiofiles
 import aiojobs
 from Bio import Entrez, SeqIO
@@ -27,6 +29,8 @@ SEQUENCE_KEYS = [
     "sequence"
 ]
 
+qualifiers = ["isolate", "strain"]
+
 Entrez.email = os.environ.get("NCBI_EMAIL")
 Entrez.api_key = os.environ.get("NCBI_API_KEY")
 
@@ -37,9 +41,12 @@ async def isolate(src):
 
     # get paths for all OTU in the directory
     paths = get_paths(src)
+
+    # get mapping of all OTU paths to their taxid
     taxids = await get_taxids(paths)
-    cache = {}
-    for path in taxids.keys():
+    unknown = 0
+    for path in paths[:200]:
+
         # only fetch OTU that have a taxid
         if taxids[path] is None:
             continue
@@ -47,63 +54,91 @@ async def isolate(src):
         # get mapping of all isolates to their folder name
         isolates = await get_isolates(path)
 
-        # approach 1
-        accessions = await check_cache(taxids[path])
+        accessions = await check_accessions_cache(taxids[path])
 
         if not accessions:
-            accessions = []
-            record = Entrez.read(Entrez.elink(dbfrom="taxonomy", db="nucleotide", id=taxids[path]))
+            accessions = {}
+            record = Entrez.read(Entrez.elink(
+                dbfrom="taxonomy", db="nucleotide", id=taxids[path], idtype="acc"))
+
             for linksetdb in record[0]["LinkSetDb"][0]["Link"]:
-                accessions.append(linksetdb["Id"])
+                accessions[linksetdb["Id"]] = None
 
-            cache_taxid(taxids[path], accessions)
+            await cache_accessions(str(taxids[path]), accessions)
+        try:
+            handle = Entrez.efetch(db="nucleotide", id=[accession for accession in accessions.keys()], rettype="gb",
+                                   retmode="text")
+        except HTTPError:
+            unknown += 1
 
-        handle = Entrez.efetch(db="nucleotide", id=accessions, rettype="gb", retmode="text")
-        record = SeqIO.parse(handle, "gb")
+        # this is somehow exponentially slower than the fetch itself
+        records = SeqIO.to_dict(SeqIO.parse(handle, "gb"))
 
-        count = []
-        for seq in record:
-            for feature in seq.features:
-                if feature.type == "source":
-                    if "isolate" not in feature.qualifiers:
-                        count.append(seq)
+        no_data = 0
+        for seq in [seq.features for seq in records.values()]:
+            isolate_data = await get_qualifiers(seq)
+            if isolate_data["isolate"] is None and isolate_data["strain"] is None:
+                no_data += 1
+        print(
+            f"Isolate source data not found in {no_data} out of {len(records)} records")
 
-        actual = []
-        print("Accessions found:", len(accessions))
-        print("Accessions with no isolate in feature qualifier:", len(count))
-        for seq in count:
-            if "isolate" not in seq.description:
-                actual.append(seq)
-        print("Accessions with no isolate in feature qualifier AND no isolate in definition:", len(actual))
+    print(f"{unknown} taxids were not found in the nucleotide database")
 
 
 async def run():
     pass
 
 
-def cache_taxid(taxid, accessions):
+async def get_qualifiers(seq):
+    f = [feature for feature in seq if feature.type == "source"]
+    isolate_data = {}
+
+    for feature in f:
+        for qualifier in qualifiers:
+            isolate_data[qualifier] = feature.qualifiers.get(qualifier)
+    return isolate_data
+
+
+async def cache_accessions(taxid, accessions):
     if not os.path.isdir(".cli"):
         os.mkdir(".cli")
-        with open(".cli/accessions_cache.json", "w") as f:
-            json.dump({}, f, indent=4)
+        cache = {taxid: accessions}
+    else:
+        async with aiofiles.open(".cli/accessions_cache.json", "r") as f:
+            cache = json.loads(await f.read())
+            cache[taxid] = accessions
 
+    async with aiofiles.open(".cli/accessions_cache.json", "w") as f:
+        f.seek(0)
+        await f.write(json.dumps(cache, indent=4))
+
+
+def cache_records(taxid, records):
     with open(".cli/accessions_cache.json", "r+") as f:
         cache = json.load(f)
-        cache[taxid] = accessions
+
+        # can't cache features since they are SeqRecord objects
+        for record in records.values():
+            new_record = {"definition": record.description,
+                          "url": f"https://www.ncbi.nlm.nih.gov/nuccore/{record.id}", "sequence": str(record.seq),
+                          "features": [str(feature) for feature in record.features if feature.type == "source"]}
+            cache[taxid][record.id] = new_record
         f.seek(0)
         json.dump(cache, f, indent=4)
 
 
-async def check_cache(taxid):
+async def check_accessions_cache(taxid):
     if not os.path.isdir(".cli"):
         return None
 
     async with aiofiles.open(".cli/accessions_cache.json", "r") as f:
         cache = json.loads(await f.read())
-        try:
-            return cache[str(taxid)]
-        except KeyError:
-            return None
+
+        return cache.get(str(taxid))
+
+
+async def check_records_cache():
+    pass
 
 
 def random_alphanumeric(length: int = 6, mixed_case: bool = False, excluded: Union[None, Iterable[str]] = None) -> str:
@@ -127,4 +162,6 @@ def random_alphanumeric(length: int = 6, mixed_case: bool = False, excluded: Uni
 
 
 if __name__ == '__main__':
-    asyncio.run(isolate("tests/files/src_b"))
+    if os.path.exists(".cli"):
+        shutil.rmtree(".cli")
+    asyncio.run(isolate("tests/files/src"))
