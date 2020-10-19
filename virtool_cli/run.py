a1e7096b07649fff076e8b961b435625d46f1a75
Jake Alexander <jake.ale@outlook.com>
2020-10-19 14:35:48 -0700

    Update test files

--- virtool_cli/run.py
+++ virtool_cli/run.py
@@ -17,7 +17,7 @@ def cli():
 @click.option("-V", "--version", default=None, type=str,
               help="the version string to include in the reference.json file")
 def build(src_path, output, indent, version):
-    """ Build a Virtool reference JSON file from a reference directory """
+    """Build a Virtool reference JSON file from a reference directory."""
     try:
         virtool_cli.build.run(src_path, output, indent, version)
     except (FileNotFoundError, NotADirectoryError):
@@ -28,33 +28,36 @@ def build(src_path, output, indent, version):
 @click.option("-src", "--src_path", required=True, type=str, help="the path to the input reference.json file")
 @click.option("-o", "--output", default="src", type=str, help="the output path for divided reference directory tree")
 def divide(src_path, output):
-    """ Divide a reference.json file from Virtool into a reference directory tree """
+    """Divide a reference.json file from Virtool into a reference directory tree."""
     try:
         if not src_path.endswith(".json"):
             raise TypeError
         virtool_cli.divide.run(src_path, output)
     except (TypeError, FileNotFoundError):
-        click.echo("Specified reference file either does not exist or is not a proper JSON file")
+        click.echo(
+            "Specified reference file either does not exist or is not a proper JSON file")
 
 
 @cli.command()
 @click.option("-src", "--src_path", required=True, type=str, help="the path to the input reference.json file")
 @click.option("-f", "--force_update", is_flag=True)
 def taxid(src_path, force_update):
-    """ Fetch taxid for all OTU in given reference directory """
+    """Fetch taxid for all OTU in given reference directory."""
     try:
         virtool_cli.taxid.run(src_path, force_update)
     except (FileNotFoundError, NotADirectoryError):
         click.echo("Not a valid reference directory")
 
+
 @cli.command()
 @click.option("-src", "--src_path", required=True, type=str, help="the path to the reference directory")
 def isolate(src_path):
-    """ Fetch new isolates for all OTU in a given reference directory """
+    """Fetch new isolates for all OTU in a given reference directory."""
     try:
         virtool_cli.isolate.run(src_path)
     except (FileNotFoundError, NotADirectoryError):
         click.echo("Not a valid reference directory")
+
 
 if __name__ == "__main__":
     cli()
