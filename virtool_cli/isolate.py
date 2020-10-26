e2087c103a549dc5336d619ebb5cd4b98a4df8d7
Jake Alexander <jake.ale@outlook.com>
2020-10-26 13:46:36 -0700

    Adjust request interval

--- virtool_cli/isolate.py
+++ virtool_cli/isolate.py
@@ -17,7 +17,7 @@ from virtool_cli.utils import get_otu_paths, get_taxid_map, get_isolates, get_se
 Entrez.email = os.environ.get("NCBI_EMAIL")
 Entrez.api_key = os.environ.get("NCBI_API_KEY")
 
-REQUEST_INTERVAL = 0.3 if Entrez.email and Entrez.api_key else 0.8
+REQUEST_INTERVAL = 0.4 if Entrez.email and Entrez.api_key else 0.6
 
 
 async def isolate(src):
