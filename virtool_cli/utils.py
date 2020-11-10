bab6a0ae770ab20e86571b4df0c826410d53e5de
Jake Alexander <jake.ale@outlook.com>
2020-11-10 13:58:43 -0800

    Lower NCBI_REQUEST_INTERVAL

--- virtool_cli/utils.py
+++ virtool_cli/utils.py
@@ -8,7 +8,7 @@ from Bio import Entrez
 Entrez.email = os.environ.get("NCBI_EMAIL")
 Entrez.api_key = os.environ.get("NCBI_API_KEY")
 
-NCBI_REQUEST_INTERVAL = 0.4 if Entrez.email and Entrez.api_key else 0.6
+NCBI_REQUEST_INTERVAL = 0.3 if Entrez.email and Entrez.api_key else 0.6
 
 
 def get_otu_paths(src_path: str) -> list:
