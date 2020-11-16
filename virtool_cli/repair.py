b0196a234ee0211f508b95d97e1a3edb905a32a8
Jake Alexander <jake.ale@outlook.com>
2020-11-16 13:21:03 -0800

    Add docstrings and types

--- virtool_cli/repair.py
+++ virtool_cli/repair.py
@@ -1,10 +1,16 @@
 import json
 import os
+from typing import Optional
 
 from virtool_cli.utils import get_otu_paths, get_otus
 
 
-def repair(src):
+def run(src: str):
+    """
+    Fixes any folder-JSON name mismatches and incorrect taxid types
+
+    :param src: Path to a given reference directory
+    """
     paths = get_otu_paths(src)
     otus = get_otus(paths)
     otus_to_update = {}
@@ -20,7 +26,13 @@ def repair(src):
     write_otus(otus_to_update)
 
 
-def fix_folder_name(path, otu):
+def fix_folder_name(path: str, otu: dict):
+    """
+    Fixes each OTU folder name by ensuring that it's the same as the "name" field in its otu.json file
+
+    :param path: Path to a given reference directory
+    :param otu: A deserialized otu.json
+    """
     try:
         folder_name = path.rsplit("/", 1)[1]
         new_folder_name = otu.get("name").replace(" ", "_").replace("/", "_").lower()
@@ -33,7 +45,13 @@ def fix_folder_name(path, otu):
         pass
 
 
-def fix_taxid(otu):
+def fix_taxid(otu: dict) -> Optional[dict]:
+    """
+    Ensures that each taxid inside every OTU's otu.json is of type int
+
+    :param otu: A deserialized otu.json
+    :return: The modified otu parameter if it needs to be updated, else None
+    """
     try:
         taxid = otu.get("taxid")
         if type(taxid) is not int and not None:
@@ -46,11 +64,12 @@ def fix_taxid(otu):
     return None
 
 
-def write_otus(otus):
+def write_otus(otus: dict):
+    """
+    Write every otu.json file that's been modified with correct taxid type
+
+    :param otus: A dictionary that maps file paths to each updated deserialized otu.json
+    """
     for path in otus.keys():
         with open(os.path.join(path, "otu.json"), "w") as f:
             json.dump(otus[path], f, indent=4)
-
-
-def run(src):
-    repair(src)
