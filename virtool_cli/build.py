12f98944dbd4bd5f49f510b98f39b44ae74d0adc
Jake Alexander <jake.ale@outlook.com>
2020-11-23 16:23:18 -0800

    Update build.py docstring format

--- virtool_cli/build.py
+++ virtool_cli/build.py
@@ -32,12 +32,10 @@ def run(src_path: pathlib.Path, output: pathlib.Path, indent: bool, version: str
     """
     Build a Virtool reference JSON file from a data directory.
 
-    Parameters:
-        src_path (str): Path to database src directory
-        output (str): The output path for the reference.json file
-        indent (bool): A flag to indicate whether the output file should be indented
-        version (str): The version string to include in the reference.json file
-
+    :param src_path: Path to database src directory
+    :param output: The output path for the reference.json file
+    :param indent: A flag to indicate whether the output file should be indented
+    :param version: The version string to include in the reference.json file
     """
     meta = parse_meta(src_path)
 
@@ -86,12 +84,8 @@ def parse_meta(src_path: pathlib.Path) -> dict:
     """
     Deserializes and returns meta.json if found, else returns an empty dictionary.
 
-    Parameters:
-        src_path (str): Path to database src directory
-
-    Returns:
-        The deserialized meta.json object or an empty dictionary.
-
+    :param src_path: Path to database src directory
+    :return: The deserialized meta.json object or an empty dictionary
     """
     try:
         with open(os.path.join(src_path, "meta.json"), "r") as f:
@@ -104,12 +98,8 @@ def parse_alpha(alpha: pathlib.Path) -> list:
     """
     Generates and returns a list with every OTU in the directory of the given alpha
 
-    Parameters:
-        alpha (str): Path to a given alpha directory in a reference
-
-    Returns:
-        A list containing all the OTU in the given directory
-
+    :param alpha: Path to a given alpha directory in a reference
+    :return: A list containing all the OTU in the given directory
     """
     return [otu for otu in alpha.iterdir()]
 
@@ -118,13 +108,8 @@ def parse_otu(otu_path: pathlib.Path) -> (dict, list):
     """
     Creates an list of isolate IDs for a given OTU
 
-    Parameters:
-        otu_path (str): Path to a OTU directory
-
-    Returns:
-        otu (dict): The dictionary for a given OTU
-        isolate_ids (list): List of all isolate IDs for a given OTU
-
+    :param otu_path: Path to a OTU directory
+    :return: The dictionary and list of isolate ids for a given OTU
     """
     with open(otu_path / "otu.json", "r") as f:
         otu = json.load(f)
@@ -140,13 +125,8 @@ def parse_isolate(isolate_path: pathlib.Path) -> (dict, list):
     """
     Creates a list of sequence IDs for a given sequence
 
-    Parameters:
-        isolate_path (str): Path to a isolate directory
-
-    Returns:
-        isolate (dict): The dictionary for a given OTU
-        sequence_ids (list): List of all sequence IDs for a given isolate
-
+    :param isolate_path: Path to a isolate directory
+    :return: The dictionary and list of sequenece ids for a given isolate
     """
     with open(isolate_path / "isolate.json", "r") as f:
         isolate = json.load(f)
