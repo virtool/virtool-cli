9f25104a0530a21dca088d319f3011777935d4ae
eroberts9789 <82687748+eroberts9789@users.noreply.github.com>
2021-06-28 19:29:18 -0700

    Refactor find_polyproteins() (#50)
    * Refactor find_polyproteins()
    * Add check_alignments_by_length()
    * Add check_alignments_by_coverage()
    * Add more descriptive naming
    * Simplify code
    
    * Update docstrings
    
    * Fix circular import
    
    Co-authored-by: Ian Boyes <igboyes@gmail.com>

--- virtool_cli/vfam_polyprotein.py
+++ virtool_cli/vfam_polyprotein.py
@@ -1,9 +1,10 @@
 from collections import defaultdict
 from pathlib import Path
-from typing import List
+
+from typing import List, Optional
 
 
-def sequence_lengths(blast_results_path: Path) -> dict:
+def get_sequence_lengths(blast_results_path: Path) -> dict:
     """
     Takes path to BLAST results file and parses through lines, creating an alignment object from each line.
 
@@ -12,7 +13,7 @@ def sequence_lengths(blast_results_path: Path) -> dict:
     :param blast_results_path: path to BLAST file produced in all_by_all blast step
     :return: sequence_lengths, a dictionary containing each sequence and its sequence length
     """
-    seq_lengths = {}
+    seq_lengths = dict()
 
     with blast_results_path.open("r") as handle:
         for line in handle:
@@ -45,42 +46,79 @@ def get_alignment_records(blast_results_path: Path) -> dict:
     return alignment_records
 
 
+def check_alignments_by_length(seq_id: str, alignment_records: dict, seq_lengths: dict) -> list:
+    """
+    Iterates through alignment records for seq_id, adds alignment record to checked_alignments if:
+
+     - The length of the subject is less than 70% the length of the query
+     - Alignment spans more than 70% of the alignment subject
+
+    :param seq_id: sequence ID for which to filter alignment records
+    :param alignment_records: dictionary containing all alignment records for each seq_id
+    :param seq_lengths: dictionary containing sequence ID: sequence length pairs
+    :return: checked alignments, a list of alignment records to investigate further in check_alignments_by_position()
+    """
+    checked_alignments = []
+
+    for alignment in alignment_records[seq_id]:
+        if seq_lengths[alignment.subject] < 0.7 * seq_lengths[alignment.query]:
+            subject_coverage = float(abs(alignment.qstart - alignment.qend)) / seq_lengths[alignment.subject]
+
+            if subject_coverage >= 0.7:
+                checked_alignments.append(alignment)
+
+    return checked_alignments
+
+
+def check_alignments_by_position(seq_id: str, checked_by_length: list, seq_lengths: dict) -> Optional[str]:
+    """
+    Iterates through alignment records for sequence ID to be further investigated from check_alignments_by_length().
+
+    If alignment queries collectively span more than 80% of sequence length, sequence is identified as polyprotein-like.
+
+    :param seq_id: sequence ID for sequence to be investigated
+    :param checked_by_length: alignments to be further investigated from check_alignment_records()
+    :param seq_lengths: dictionary containing sequence ID: sequence length pairs
+    :return: seq_id if found polyprotein-like
+    """
+    query_coverage = dict()
+
+    for alignment in checked_by_length:
+        for position in range(alignment.qstart, alignment.qend):
+            query_coverage[position] = None
+
+    if len(query_coverage) > 0.8 * (seq_lengths[seq_id]):
+        return seq_id
+
+    return None
+
+
 def find_polyproteins(blast_results_path: Path) -> List[str]:
     """
     Sequences are filtered by sequence length and coverage to determine if they are polyprotein_ids/polyprotein-like.
 
     Sequences longer than 400 amino acids in length were identified as polyprotein or polyprotein-like if
-
     - at least 70% of the sequence length was covered by two or more other proteins in the sequence set.
     - these two or more other proteins were covered at least 80% by the longer sequence.
 
     :param blast_results_path: path to BLAST file produced in blast_all_by_all() step
     :return: polyprotein_ids, a list of sequences to not include in output
+
     """
-    seq_lengths = sequence_lengths(blast_results_path)
+    seq_lengths = get_sequence_lengths(blast_results_path)
     alignment_records = get_alignment_records(blast_results_path)
     polyprotein_ids = []
 
-    for query in seq_lengths:
-        if seq_lengths[query] > 400 and query in alignment_records:
+    for seq_id in seq_lengths:
+        if seq_lengths[seq_id] > 400 and seq_id in alignment_records:
 
-            alignment_ranges = []
+            checked_by_length = check_alignments_by_length(seq_id, alignment_records, seq_lengths)
 
-            for alignment in alignment_records[query]:
-                if alignment.subject in seq_lengths:
-                    if seq_lengths[alignment.subject] < 0.7 * seq_lengths[alignment.query]:
-                        subject_cvg = float(abs(alignment.qstart - alignment.qend))/seq_lengths[alignment.subject]
-                        if subject_cvg >= 0.7:
-                            alignment_ranges.append((alignment.qstart, alignment.qend))
+            if checked_by_length:
+                checked_by_position = check_alignments_by_position(seq_id, checked_by_length, seq_lengths)
 
-            query_cvg = {}
-            for rng in alignment_ranges:
-                for position in range(rng[0], rng[1]):
-                    query_cvg[position] = None
-
-            if len(query_cvg) > 0.8 * (seq_lengths[query]):
-                polyprotein_ids.append(query)
-            query_cvg.clear()
+                if checked_by_position:
+                    polyprotein_ids.append(checked_by_position)
 
     return polyprotein_ids
 
