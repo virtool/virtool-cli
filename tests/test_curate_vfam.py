1138ea228f2d97298feae7054b147c1bc101a37c
Elizabeth <eroberts9789@gmail.com>
2021-07-06 16:12:42 -0700

    Update logging for gathering taxonomic information * Add error message for case where NCBI doesn't find record * Add message containing number of families and genus found

--- tests/test_curate_vfam.py
+++ tests/test_curate_vfam.py
@@ -6,7 +6,7 @@ import pytest
 from Bio import SeqIO
 
 from paths import VFAM_INPUT_PATH, VFAM_INTERMEDIATES_PATH
-from virtool_cli.vfam_curate import group_input_paths, write_curated_recs
+from virtool_cli.vfam_curate import group_input_paths
 
 DUPES_INPUT = VFAM_INPUT_PATH / "Dupes"
 GENERIC_INPUT = VFAM_INPUT_PATH / "Generic"
