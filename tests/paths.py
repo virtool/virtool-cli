03441bfb2745cd6c3c402ae9df0adc33e7264014
Ian Boyes <igboyes@gmail.com>
2021-09-28 14:01:32 -0700

    Reorganize test data files

--- tests/paths.py
+++ tests/paths.py
@@ -1,5 +1,5 @@
 from pathlib import Path
 
-TEST_FILES_PATH = Path(__file__).parent.parent / "tests" / "files"
-VFAM_INPUT_PATH = Path(__file__).parent.parent / "tests" / "vfam_input"
-VFAM_INTERMEDIATES_PATH = Path(__file__).parent.parent / "tests" / "vfam_intermediates"
+TEST_FILES_PATH = Path(__file__).parent / "files"
+VFAM_INPUT_PATH = TEST_FILES_PATH / "vfam_input"
+VFAM_INTERMEDIATES_PATH = TEST_FILES_PATH / "vfam_intermediates"
