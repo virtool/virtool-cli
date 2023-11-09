0bc7c6964589950139c69906f9497da8a4be0d98
Shelley Gao <syugao@gmail.com>
2023-11-08 17:12:23 -0800

    Added more test cases

--- tests/test_check.py
+++ tests/test_check.py
@@ -5,13 +5,20 @@ from paths import TEST_FILES_PATH
 TEST_SRC_PATH = TEST_FILES_PATH / "src_test"
 TEST_BAD_SRC_PATH = TEST_FILES_PATH / "src_malformed"
 
+TEST_DIRS = [
+    "abaca_bunchy_top_virus--c93ec9a9",
+    "babaco_mosaic_virus--xcl20vqt",
+    "cabbage_leaf_curl_jamaica_virus--d226290f",
+    "faba_bean_necrotic_stunt_alphasatellite_1--6444acf3",
+]
+
 
 @pytest.fixture()
 def command(otu_path):
     return ["virtool", "ref", "check", "otu", "--otu_path", str(otu_path)]
 
 
-@pytest.mark.parametrize("otu_dir", ["abaca_bunchy_top_virus--c93ec9a9"])
+@pytest.mark.parametrize("otu_dir", TEST_DIRS)
 def test_otu_check_success(otu_dir):
     otu_path = TEST_SRC_PATH / otu_dir
     output = subprocess.check_output(
@@ -21,7 +28,7 @@ def test_otu_check_success(otu_dir):
     assert output.strip() == "True"
 
 
-@pytest.mark.parametrize("otu_dir", ["abaca_bunchy_top_virus--c93ec9a9"])
+@pytest.mark.parametrize("otu_dir", TEST_DIRS)
 def test_otu_check_fail(otu_dir):
     otu_path = TEST_BAD_SRC_PATH / otu_dir
     output = subprocess.check_output(
