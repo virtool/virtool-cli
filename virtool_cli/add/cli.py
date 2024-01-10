0d41342f55f93ebac1013e458072c8c798454449
Shelley Gao <sygao@uvic.ca>
2024-01-10 14:21:57 -0800

    redesigned built test; Removed catalog references from add.accession

--- virtool_cli/add/cli.py
+++ virtool_cli/add/cli.py
@@ -1,7 +1,6 @@
 from pathlib import Path
 import click
 
-from virtool_cli.add.accession import run_single as run_add_accession
 from virtool_cli.add.accession import run_multiple as run_add_accessions
 from virtool_cli.add.otu import run as run_add_otu
 
@@ -13,37 +12,37 @@ def add():
     """
     pass
 
-
-@add.command()
-@click.option(
-    "-acc",
-    "--accession",
-    required=True,
-    type=str,
-    help="An accession from the NCBI Nucleotide database",
-)
-@click.option(
-    "-src",
-    "--src_path",
-    required=True,
-    type=click.Path(exists=True, file_okay=False, path_type=Path),
-    help="the path to a reference directory",
-)
-@click.option(
-    "-cat",
-    "--catalog_path",
-    required=False,
-    type=click.Path(exists=True, file_okay=False, path_type=Path),
-    default=".cache/catalog",
-    help="the path to a catalog directory",
-)
-@click.option("--debug/--no-debug", default=False, help="Enable debugging logs")
-def accession(accession, src_path, catalog_path, debug):
-    """Takes an accession and retrieves the data."""
-    try:
-        run_add_accession(accession, src_path, catalog_path, debugging=debug)
-    except (FileNotFoundError, NotADirectoryError):
-        click.echo("Not a valid reference directory", err=True)
+#
+# @add.command()
+# @click.option(
+#     "-acc",
+#     "--accession",
+#     required=True,
+#     type=str,
+#     help="An accession from the NCBI Nucleotide database",
+# )
+# @click.option(
+#     "-src",
+#     "--src_path",
+#     required=True,
+#     type=click.Path(exists=True, file_okay=False, path_type=Path),
+#     help="the path to a reference directory",
+# )
+# @click.option(
+#     "-cat",
+#     "--catalog_path",
+#     required=False,
+#     type=click.Path(exists=True, file_okay=False, path_type=Path),
+#     default=".cache/catalog",
+#     help="the path to a catalog directory",
+# )
+# @click.option("--debug/--no-debug", default=False, help="Enable debugging logs")
+# def accession(accession, src_path, catalog_path, debug):
+#     """Takes an accession and retrieves the data."""
+#     try:
+#         run_add_accession(accession, src_path, catalog_path, debugging=debug)
+#     except (FileNotFoundError, NotADirectoryError):
+#         click.echo("Not a valid reference directory", err=True)
 
 
 @add.command()
