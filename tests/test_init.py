b437b4a9b921696dab6c538ec7c8748cccc73700
Ian Boyes <igboyes@gmail.com>
2024-01-25 07:54:41 -0800

    feat: simplify `ref init` command
    * Accept `--path` for the location of the new reference.
    * Extend testing.
    * Reduce logging.
    * Create `.gitignore`.
    * Make `--debug` a flag.

--- tests/test_init.py
+++ tests/test_init.py
@@ -1,33 +1,71 @@
+import json
+from pathlib import Path
+
 import pytest
-import subprocess
+
+from virtool_cli.init import init_reference
 
 
 @pytest.fixture()
-def empty_repo(tmp_path):
-    new_repo_path = tmp_path / "new_repo"
+def assert_repo_valid():
+    """Provides a function that asserts that a reference repository is valid."""
 
-    subprocess.call(["virtool", "ref", "init", "-repo", str(new_repo_path)])
+    def func(path):
+        assert (path / "src").exists()
+        assert (path / ".cache").exists()
+        assert (path / ".github").exists()
 
-    return new_repo_path
+        with open(path / "src" / "meta.json") as f:
+            assert json.load(f) == {"data_type": "genome"}
+
+    return func
 
 
-def test_init_exist(empty_repo):
-    assert empty_repo.exists()
+def test_init_empty(assert_repo_valid, log, tmp_path: Path):
+    """Test that a new reference repository is created when the target path does not
+    exist.
+    """
+    path = tmp_path / "new_repo"
+
+    init_reference(path)
+
+    assert_repo_valid(path)
+
+    assert log.has("Complete")
 
 
-def test_init_build(empty_repo):
-    build_path = empty_repo / "reference.json"
+def test_init_exists(assert_repo_valid, log, tmp_path: Path):
+    """Test that a new reference repository can be created in an existing empty
+    directory.
+    """
+    path = tmp_path / "new_repo"
+    path.mkdir()
 
-    subprocess.call(
-        [
-            "virtool",
-            "ref",
-            "build",
-            "-o",
-            str(build_path),
-            "-src",
-            str(empty_repo / "src"),
-        ]
-    )
+    init_reference(path)
 
-    assert build_path.exists()
+    assert_repo_valid(path)
+
+    assert log.has("Complete")
+
+
+def test_init_not_empty(log, tmp_path: Path):
+    """Test that reference initialization fails when the target path is not empty."""
+    path = tmp_path / "new_repo"
+    path.mkdir()
+    (path / "file.txt").touch()
+
+    init_reference(path)
+
+    assert log.has("The directory is not empty")
+    assert not log.has("Complete")
+
+
+def test_init_is_file(log, tmp_path: Path):
+    """Test that reference initialization fails when the target path is a file."""
+    path = tmp_path / "new_repo"
+    path.touch()
+
+    init_reference(path)
+
+    assert log.has("The target path is a file")
+    assert not log.has("Complete")
