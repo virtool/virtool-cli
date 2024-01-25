7e6c4a9aefbb97cf1569ea585036b890abd752b8
Ian Boyes <igboyes@gmail.com>
2024-01-25 14:38:38 -0800

    refactor: cleanup `add` module
    * Remove layer between CLI and business functions.
    * Standardize on `--path` referring to the repo path.
    * Get rid of underscores in args (`--otu_path`)
    * Consolidate files.
    * Extract some `click` options into named decorators.
    * Use `click.argument` instead of `option` where appropriate.

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,8 @@ from pathlib import Path
 
 import pytest
 
+from virtool_cli.ref.init import init_reference
+
 
 @pytest.fixture()
 def output(tmp_path):
@@ -30,5 +32,15 @@ def src_test_path(test_files_path: Path):
 def src_scratch_path(src_test_path: Path, tmp_path: Path):
     path = tmp_path / "src_scratch"
     shutil.copytree(src_test_path, path)
+
+    return path
+
+
+@pytest.fixture()
+def scratch_path(src_test_path: Path, tmp_path: Path):
+    path = tmp_path / "reference"
+    init_reference(path)
+
+    shutil.copytree(src_test_path, path / "src", dirs_exist_ok=True)
 
     return path
--- virtool_cli/add/cli.py
+++ virtool_cli/add/cli.py
@@ -1,92 +1,90 @@
+import asyncio
 from pathlib import Path
+
 import click
 
-from virtool_cli.add.add_accession_auto import run as run_add_accession
-from virtool_cli.add.add_accessions import run as run_add_accessions
-from virtool_cli.add.add_otu import run as run_add_otu
+from virtool_cli.add.accessions import add_accession, add_accessions
+from virtool_cli.add.otus import add_otu
+from virtool_cli.options import debug_option, path_option
+from virtool_cli.utils.logging import configure_logger
 
 
 @click.group("add")
 def add():
-    """
-    Commands related to new data
-    """
-    pass
+    """Commands related to new data"""
 
 
 @add.command()
-@click.option(
-    "-acc",
-    "--accession",
-    required=True,
+@click.argument(
+    "accession_",
+    metavar="ACCESSION",
     type=str,
-    help="An accession from the NCBI Nucleotide database",
 )
-@click.option(
-    "-src",
-    "--src_path",
-    required=True,
-    type=click.Path(exists=True, file_okay=False, path_type=Path),
-    help="the path to a reference directory",
-)
-@click.option("--debug/--no-debug", default=False, help="Enable debugging logs")
-def accession(accession, src_path, debug):
-    """Takes an accession and retrieves the data."""
+@path_option
+@debug_option
+def accession(accession_: str, debug: bool, path: Path):
+    """Retrieves data for the accession."""
+    configure_logger(debug)
+
     try:
-        run_add_accession(accession, src_path, debugging=debug)
+        asyncio.run(add_accession(accession_, path))
     except (FileNotFoundError, NotADirectoryError):
         click.echo("Not a valid reference directory", err=True)
 
 
 @add.command()
-@click.option(
-    "-acc",
-    "--accessions",
-    required=True,
+@click.argument(
+    "accessions_",
+    metavar="ACCESSIONS",
+    nargs=-1,
     type=str,
-    help="An accession from the NCBI Nucleotide database",
 )
 @click.option(
-    "-otu",
-    "--otu_path",
-    required=True,
+    "--otu-path",
     type=click.Path(exists=True, file_okay=False, path_type=Path),
-    help="the path to a reference directory",
 )
-@click.option("--debug/--no-debug", default=False, help="Enable debugging logs")
-def accessions(accessions, otu_path, debug):
-    """Takes a list of comma-delineated accessions and retrieves the data."""
+@debug_option
+def accessions(accessions_: list[str], otu_path, debug):
+    """Fetch and write the data for the given NCBI accessions to an OTU.
+
+    virtool add accessions --otu-path ./tmv MN908947.3 MN996528.1
+
+    """
+    configure_logger(debug)
+
     try:
-        run_add_accessions(accessions, otu_path, debug)
+        asyncio.run(add_accessions(list(accessions_), otu_path))
     except (FileNotFoundError, NotADirectoryError):
-        click.echo("Not a valid reference directory", err=True)
+        click.echo("could not find otu directory", err=True)
 
 
 @add.command()
-@click.option(
-    "-taxid",
-    "--taxon_id",
-    required=True,
+@click.argument(
+    "taxid",
     type=int,
-    help="A unique identifier from the NCBI Taxonomy database",
 )
-@click.option(
-    "-src",
-    "--src_path",
-    required=True,
-    type=click.Path(exists=True, file_okay=False, path_type=Path),
-    help="the path to a reference directory",
-)
-@click.option("--debug/--no-debug", default=False, help="Enable debugging logs")
-def otu(taxon_id, src_path, debug):
-    """Create a new taxon ID and populate with accessions."""
+@debug_option
+@path_option
+def otu(debug: bool, path: Path, taxid: int):
+    """Add a new OTU using a NCBI taxonomy ID."""
+    configure_logger(debug)
 
     try:
-        run_add_otu(taxon_id, src_path, debug)
-
+        asyncio.run(add_otu(taxid, path))
     except (FileNotFoundError, NotADirectoryError):
         click.echo("Not a valid OTU directory", err=True)
 
 
-if __name__ == "__main__":
-    add()
+def split_clean_csv_string(input_string: str, delimiter: str = ",") -> list[str]:
+    """Splits comma-separated values into a list of strings.
+
+    :param input_string: A raw comma-delineated string
+    :param delimiter: A delimiter string for use with split(). Defaults to a comma (",")
+    :return: A list of accession strings
+    """
+    raw_list = input_string.split(delimiter)
+    stripped_list = []
+    for item in raw_list:
+        stripped_list.append(item.strip())
+
+    return stripped_list
