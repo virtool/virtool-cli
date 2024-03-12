99a86c0f36059e668efeb4fe9f7d8b5af94d9f4c
Shelley Gao <syugao@gmail.com>
2024-03-12 12:40:02 -0700

    Removed unused IncompleteRecordsError

--- virtool_cli/ncbi/error.py
+++ virtool_cli/ncbi/error.py
@@ -3,15 +3,6 @@ class NCBIClientError(Exception):
         self.message = message
 
 
-class IncompleteRecordsError(NCBIClientError):
-    def __init__(self, message: str = "", data: list[dict] = None):
-        super().__init__(message)
-        if data is None:
-            self.data = []
-        else:
-            self.data = data
-
-
 class NCBIParseError(Exception):
     def __init__(self, keys, message: str = ""):
         self.keys = list(keys)
