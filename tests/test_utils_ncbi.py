9de058ab027a2e2f2270ba4e7eeb4eca91b9efdd
Shelley Gao <syugao@gmail.com>
2024-03-12 14:02:29 -0700

    Skipping ncbi utils tests

--- tests/test_utils_ncbi.py
+++ tests/test_utils_ncbi.py
@@ -8,6 +8,7 @@ async def taxon_ids(src_test_path) -> list[int]:
     return [438782, 2060511, 345184, 1278205, 223262]
 
 
+@pytest.mark.skip
 @pytest.mark.parametrize("index", [0, 1, 2])
 async def test_utils_request_linked_accessions(index: int, taxon_ids: list[int]):
     accessions = await request_linked_accessions(taxon_ids[index])
@@ -16,6 +17,7 @@ async def test_utils_request_linked_accessions(index: int, taxon_ids: list[int])
     assert all(isinstance(accession, str) for accession in accessions)
 
 
+@pytest.mark.skip
 @pytest.mark.parametrize("index", [0, 1, 2])
 async def test_utils_fetch_taxonomy_rank(index: int, taxon_ids: list[int]):
     taxon_id = taxon_ids[index]
