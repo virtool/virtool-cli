a3e3030fcd5e9a9b95feed84cedbec7361863093
Shelley Gao <syugao@gmail.com>
2024-04-10 13:51:58 -0700

    Docstrings

--- virtool_cli/ref/utils.py
+++ virtool_cli/ref/utils.py
@@ -10,6 +10,8 @@ class DataType(StrEnum):
 
 
 class MolType(StrEnum):
+    """The in vivo molecule type of a sequence, corresponds to Genbank's moltype field"""
+
     DNA = "DNA"
     RNA = "RNA"
     TRNA = "tRNA"
@@ -18,17 +20,25 @@ class MolType(StrEnum):
 
 
 class Strandedness(StrEnum):
+    """Strandedness of a molecule, either single or double"""
+
     SINGLE = "single"
     DOUBLE = "double"
 
 
 class Topology(StrEnum):
+    """Topology of a molecule, either linear or circular"""
+
     LINEAR = "linear"
     CIRCULAR = "circular"
 
 
 @dataclass
 class Molecule:
+    """The strandedness, molecule type and topology of this OTU.
+
+    Corresponds to all sequences found in this OTU."""
+
     strandedness: Strandedness
     type: MolType
     topology: Topology
