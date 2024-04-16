e74f6c762977e68794eb3f45f26bbbb7f6e3349e
Ian Boyes <igboyes@gmail.com>
2024-04-16 16:09:35 -0700

    test: consolidate fixtures

--- tests/test_ncbi_model.py
+++ tests/test_ncbi_model.py
@@ -6,11 +6,6 @@ from virtool_cli.ncbi.cache import NCBICache
 from virtool_cli.ncbi.model import NCBIGenbank, NCBILineage, NCBIRank, NCBITaxonomy
 
 
-@pytest.fixture()
-def scratch_cache(cache_scratch_path):
-    return NCBICache(cache_scratch_path)
-
-
 class TestParseGenbank:
     @pytest.mark.parametrize(
         "accession",
@@ -19,33 +14,33 @@ class TestParseGenbank:
     def test_ok(
         self,
         accession: str,
-        scratch_cache,
+        scratch_ncbi_cache: NCBICache,
         snapshot: SnapshotAssertion,
     ):
         """Test that multiple valid records can be parsed successfully."""
-        record = scratch_cache.load_genbank_record(accession)
+        record = scratch_ncbi_cache.load_genbank_record(accession)
         assert NCBIGenbank(**record).model_dump() == snapshot
 
     def test_source(
         self,
-        scratch_cache,
+        scratch_ncbi_cache: NCBICache,
         snapshot: SnapshotAssertion,
     ):
         """Test that the source table is correctly extracted from the feature table."""
-        record = scratch_cache.load_genbank_record("AB017504")
+        record = scratch_ncbi_cache.load_genbank_record("AB017504")
         assert NCBIGenbank(**record).source.model_dump() == snapshot
 
     def test_taxid(
         self,
-        scratch_cache,
+        scratch_ncbi_cache,
     ):
         """Test that the taxid is correctly extracted from the source field."""
-        record = scratch_cache.load_genbank_record("AB017504")
+        record = scratch_ncbi_cache.load_genbank_record("AB017504")
         assert NCBIGenbank(**record).source.taxid == 1169032
 
-    def test_sequence_validation_fail(self, scratch_cache: NCBICache):
+    def test_sequence_validation_fail(self, scratch_ncbi_cache: NCBICache):
         """Test that validation fails when the sequence contains invalid characters."""
-        record = scratch_cache.load_genbank_record("AB017504")
+        record = scratch_ncbi_cache.load_genbank_record("AB017504")
 
         assert NCBIGenbank(**record)
 
@@ -63,11 +58,11 @@ class TestParseTaxonomy:
     def test_ok(
         self,
         taxid: int,
-        scratch_cache: NCBICache,
+        scratch_ncbi_cache: NCBICache,
         snapshot: SnapshotAssertion,
     ):
         """Test that multiple valid records can be parsed successfully."""
-        record = scratch_cache.load_taxonomy(taxid)
+        record = scratch_ncbi_cache.load_taxonomy(taxid)
 
         taxonomy = NCBITaxonomy(**record)
 
@@ -76,13 +71,13 @@ class TestParseTaxonomy:
 
     def test_with_rank(
         self,
-        scratch_cache: NCBICache,
+        scratch_ncbi_cache: NCBICache,
         snapshot: SnapshotAssertion,
     ):
         """Test that the rank field can be explicitly set using a kwarg in the case that
         it is not present in the record.
         """
-        record = scratch_cache.load_taxonomy(1016856)
+        record = scratch_ncbi_cache.load_taxonomy(1016856)
 
         with pytest.raises(ValidationError):
             assert NCBITaxonomy(**record)
@@ -93,7 +88,7 @@ class TestParseTaxonomy:
         assert taxonomy.model_dump() == snapshot
 
 
-def test_create_lineage_item_alias(snapshot: SnapshotAssertion):
+def test_create_lineage_item_alias():
     """Test that the NCBILineage model can be created with or without aliases."""
     lineage_data_with_aliases = {
         "TaxId": "2732397",
