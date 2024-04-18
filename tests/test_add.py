7d25d1c84aa1711742dbdaf2f77e8364231c3f5a
Shelley Gao <syugao@gmail.com>
2024-04-18 15:38:45 -0700

    Revising Add

--- tests/test_add.py
+++ tests/test_add.py
@@ -1,16 +1,53 @@
-import asyncio
+import shutil
 import json
 import subprocess
 from pathlib import Path
 
+import orjson
 import pytest
 from syrupy import SnapshotAssertion
-from syrupy.filters import props
+from syrupy.filters import props, paths
 
-from virtool_cli.add.accessions import add_accession
-from virtool_cli.ref.build import build_json
-from virtool_cli.ref.init import init_reference
+from virtool_cli.ref.otu import add_otu
+
+# from virtool_cli.ref.build import build_json
+# from virtool_cli.ref.init import init_reference
+from virtool_cli.ref.repo import EventSourcedRepo as Repo
 from virtool_cli.utils.reference import get_sequence_paths
+
+
+@pytest.fixture
+def empty_repo_path(tmp_path):
+    yield tmp_path / "empty_repo"
+
+
+@pytest.fixture
+def precached_repo(empty_repo_path, cache_example_path):
+    cache_path = empty_repo_path / ".cache"
+
+    subprocess.run(
+        [
+            "virtool",
+            "ref",
+            "init",
+            "--data-type",
+            "genome",
+            "--name",
+            "empty repo",
+            "--organism",
+            "virus",
+            "--path",
+            str(empty_repo_path),
+        ],
+        check=False,
+    )
+
+    shutil.copytree(cache_example_path, (cache_path / "ncbi"), dirs_exist_ok=True)
+    (cache_path / "repo").mkdir(exist_ok=True)
+
+    yield Repo(empty_repo_path)
+
+    shutil.rmtree(empty_repo_path)
 
 
 def get_all_sequence_paths(otu_path: Path) -> set[Path]:
@@ -22,265 +59,128 @@ def get_all_sequence_paths(otu_path: Path) -> set[Path]:
     }
 
 
-@pytest.mark.ncbi
-class TestAddAccession:
-    @staticmethod
-    def get_all_filenames_in_dir(path: Path) -> set[str]:
-        ignore_list = [".DS_Store", "Thumbs.db"]
-
-        return {p.name for p in path.iterdir() if p.name not in ignore_list}
-
-    def test_add_new_isolate(
-        self,
-        scratch_path: Path,
-        snapshot: SnapshotAssertion,
-    ):
-        """Test that an accession is added when the parent isolate already exists."""
-        otu_path = scratch_path / "src" / "cabbage_leaf_curl_jamaica_virus--d226290f"
-
-        asyncio.run(add_accession("NC_038793", scratch_path))
-
-        new_isolate_filenames = self.get_all_filenames_in_dir(otu_path) - {
-            "496550f5",
-            "d293d531",
-            "exclusions.json",
-            "otu.json",
-        }
-
-        assert len(new_isolate_filenames) == 1
-
-        isolate_id = new_isolate_filenames.pop()
-
-        with open(otu_path / isolate_id / "isolate.json") as f:
-            assert json.load(f) == {
-                "default": False,
-                "id": isolate_id,
-                "source_name": "CUc-3",
-                "source_type": "isolate",
-            }
-
-        new_sequence_filenames = self.get_all_filenames_in_dir(
-            otu_path / isolate_id
-        ) - {
-            "isolate.json",
-        }
-
-        assert len(new_sequence_filenames) == 1
-
-        sequence_id = new_sequence_filenames.pop().replace(".json", "")
-
-        with open(otu_path / isolate_id / f"{sequence_id}.json") as f:
-            data = json.load(f)
-            assert data == snapshot(name="sequence.json", exclude=props("_id"))
-            assert data["_id"] == sequence_id
-            assert data["accession"] == "NC_038793.1"
-
-    def test_add_to_existing_isolate(
-        self,
-        scratch_path: Path,
-        snapshot: SnapshotAssertion,
-    ):
-        """Test that an accession is added when the parent isolate already exists."""
-        asyncio.run(add_accession("DQ178612", scratch_path))
-
-        otu_path = scratch_path / "src" / "cabbage_leaf_curl_jamaica_virus--d226290f"
-
-        assert not (
-            self.get_all_filenames_in_dir(otu_path)
-            - {
-                "496550f5",
-                "d293d531",
-                "exclusions.json",
-                "otu.json",
-            }
-        )
-
-        new_sequence_filenames = self.get_all_filenames_in_dir(
-            otu_path / "d293d531"
-        ) - {
-            "isolate.json",
-            "ndaxyl7f.json",
-            "pyf9x4wh.json",
-        }
-
-        assert len(new_sequence_filenames) == 1
-
-        sequence_id = new_sequence_filenames.pop().replace(".json", "")
-
-        with open(otu_path / "d293d531" / f"{sequence_id}.json") as f:
-            data = json.load(f)
-            assert data == snapshot(name="sequence.json", exclude=props("_id"))
-            assert data["_id"] == sequence_id
-
-    def test_accession_is_invalid(self, scratch_path: Path):
-        """Test that adding an accession fails when it is invalid."""
-        otu_path = (
-            scratch_path / "src" / "pagoda_yellow_mosaic_associated_virus--dd21fd8f"
-        )
-
-        before = get_all_sequence_paths(otu_path)
-
-        asyncio.run(add_accession("NC-024301", scratch_path))
-
-        assert get_all_sequence_paths(otu_path) == before
-
-    def test_accession_already_exists(
-        self,
-        scratch_path: Path,
-    ):
-        """Test that an accession cannot be added if it already exists."""
-        otu_path = scratch_path / "src" / "abaca_bunchy_top_virus--c93ec9a9"
-
-        before = get_all_sequence_paths(otu_path)
-
-        asyncio.run(add_accession("NC_010319", scratch_path))
-
-        assert get_all_sequence_paths(otu_path) == before
-
-
-@pytest.mark.ncbi
-class TestAddAccessions:
-    @staticmethod
-    def run_add_accessions(accessions: list[str], otu_dirname: str, path: Path):
-        """Add into an existing isolate directory"""
-        otu_path = path / "src" / otu_dirname
-
-        pre_sequence_paths = get_all_sequence_paths(otu_path)
-
-        subprocess.run(
-            [
-                "virtool",
-                "ref",
-                "add",
-                "accessions",
-                "--otu-path",
-                str(otu_path),
-                *accessions,
-            ],
-            check=False,
-        )
-
-        return pre_sequence_paths, get_all_sequence_paths(otu_path)
-
-    @pytest.mark.parametrize(
-        "accessions, otu_dirname",
-        [
-            (["DQ178612, NC_038793"], "cabbage_leaf_curl_jamaica_virus--d226290f"),
-            (["KT390494, KT390496, KT390501"], "nanovirus_like_particle--ae0f2a35"),
-        ],
-    )
-    def test_success(
-        self,
-        accessions: list[str],
-        otu_dirname: str,
-        scratch_path: Path,
-    ):
-        """Check that virtool ref add accessions does the job"""
-        pre_sequence_paths, post_sequence_paths = self.run_add_accessions(
-            accessions,
-            otu_dirname,
-            scratch_path,
-        )
-
-        assert post_sequence_paths - pre_sequence_paths
-
-    @pytest.mark.parametrize(
-        "accessions, otu_dirname",
-        [
-            (["DQ178611, DQ178610"], "cabbage_leaf_curl_jamaica_virus--d226290f"),
-            (["KTX390494, KTX390496, KTX390501"], "nanovirus_like_particle--ae0f2a35"),
-        ],
-    )
-    def test_failure(
-        self,
-        accessions: list[str],
-        otu_dirname: str,
-        scratch_path: Path,
-    ):
-        """Check that virtool ref add accessions does the job"""
-        pre_sequence_paths, post_sequence_paths = self.run_add_accessions(
-            accessions,
-            otu_dirname,
-            scratch_path,
-        )
-
-        assert post_sequence_paths == pre_sequence_paths
-
-
-@pytest.mark.ncbi
-class TestAddOTU:
-    @staticmethod
-    def run_add_otu(path: Path, taxid: int):
-        subprocess.run(
-            [
-                "virtool",
-                "ref",
-                "add",
-                "otu",
-                "--path",
-                str(path),
-                str(taxid),
-            ],
-            check=False,
-        )
-
-    def test_success(self, scratch_path: Path):
-        """ """
-        pre_otu_paths = set((scratch_path / "src").glob("*--*"))
-
-        self.run_add_otu(scratch_path, 908125)
-
-        new_otu_paths = set((scratch_path / "src").glob("*--*")) - pre_otu_paths
-
-        assert len(new_otu_paths) == 1
-        assert (new_otu_paths.pop() / "otu.json").exists()
-
-    def test_failure(self, scratch_path: Path):
-        """Test that adding an OTU with a pre-existing taxid fails."""
-        pre_otu_paths = set((scratch_path / "src").glob("*--*"))
-
-        self.run_add_otu(scratch_path, 345184)
-
-        assert not set((scratch_path / "src").glob("*--*")) - pre_otu_paths
-
-
-@pytest.mark.parametrize(
-    "taxon_id,accession",
-    [(908125, "NC_031754"), (1016856, "NC_015504")],
-)
-def test_init_and_add(taxon_id: int, accession: str, tmp_path: Path):
-    path = tmp_path / "empty"
-
-    init_reference(path)
-
-    subprocess.run(
-        [
-            "virtool",
-            "ref",
-            "add",
-            "otu",
-            "--path",
-            str(path),
-            str(taxon_id),
-        ],
-        check=False,
-    )
-
-    build_path = tmp_path / "reference.json"
-
-    build_json(False, build_path, path, "")
-
+def run_build_reference(repo: Path, output: Path):
     subprocess.run(
         [
             "virtool",
             "ref",
             "build",
-            "-o",
-            str(build_path),
             "--path",
-            str(path),
+            str(repo),
+            "--output-path",
+            str(output),
         ],
-        check=True,
+        check=False,
     )
 
-    assert build_path.exists()
+
+class TestAddOTU:
+    @staticmethod
+    def run_add_otu(taxid: int, accessions: list[str], path: Path):
+        command = [
+            "virtool",
+            "ref",
+            "otu",
+            "init",
+            "--path",
+            str(path),
+            "--taxid",
+            str(taxid),
+        ]
+
+        subprocess.run([*command, *accessions])
+
+    @pytest.mark.parametrize(
+        "taxid,accessions",
+        [
+            (
+                438782,
+                [
+                    "NC_010314",
+                    "NC_010315",
+                    "NC_010316",
+                    "NC_010317",
+                    "NC_010318",
+                    "NC_010319",
+                ],
+            ),
+        ],
+    )
+    def test_success(
+        self, taxid, accessions, precached_repo, snapshot: SnapshotAssertion
+    ):
+        self.run_add_otu(taxid, accessions, precached_repo.path)
+
+        run_build_reference(repo=precached_repo.path, output=precached_repo.path)
+
+        built_reference_path = precached_repo.path / "reference.json"
+
+        with open(built_reference_path, "rb") as f:
+            built_reference = orjson.loads(f.read())
+
+        for otu in built_reference["otus"]:
+            assert otu == snapshot(exclude=props("id", "isolates"))
+
+            for isolate in otu["isolates"]:
+                assert isolate == snapshot(exclude=props("id", "sequences"))
+
+                indexed_sequences = {
+                    sequence["accession"]: sequence for sequence in isolate["sequences"]
+                }
+
+                for accession in sorted(indexed_sequences):
+                    assert indexed_sequences[accession] == snapshot(exclude=paths("id"))
+
+
+@pytest.mark.ncbi
+class TestAddOTUAutofill:
+    @staticmethod
+    def run_add_otu(taxid: int, path: Path):
+        subprocess.run(
+            ["virtool", "ref", "otu", "create", str(taxid), "--path", str(path)],
+            check=False,
+        )
+
+    @staticmethod
+    def run_build_reference(repo: Path, output: Path):
+        subprocess.run(
+            [
+                "virtool",
+                "ref",
+                "build",
+                "--path",
+                str(repo),
+                "--output-path",
+                str(output),
+            ],
+            check=False,
+        )
+
+    @staticmethod
+    def read_build(path) -> dict:
+        with open(path, "rb") as f:
+            return orjson.loads(f.read())
+
+    @pytest.mark.parametrize("taxid", [438782])
+    def test_success(self, taxid, precached_repo: Repo):
+        print(list((precached_repo.path / ".cache").iterdir()))
+        output_path = precached_repo.path / "reference.json"
+
+        self.run_build_reference(precached_repo.path, precached_repo.path)
+        pre_repo = self.read_build(output_path)
+
+        assert pre_repo["otus"] == []
+
+        self.run_add_otu(taxid=taxid, path=precached_repo.path)
+
+        self.run_build_reference(precached_repo.path, precached_repo.path)
+
+        assert output_path.exists()
+        post_repo = self.read_build(output_path)
+
+        assert post_repo["otus"]
+
+        for key in ["created_at", "data_type", "name"]:
+            assert pre_repo[key] == post_repo[key]
+
+        assert post_repo["otus"] != pre_repo["otus"]
--- virtool_cli/add/cli.py
+++ virtool_cli/add/cli.py
@@ -3,10 +3,9 @@ from pathlib import Path
 
 import click
 
-from virtool_cli.add.accessions import add_accessions
-from virtool_cli.add.otu import add_otu
+from virtool_cli.add.accessions import add_accession, add_accessions
+from virtool_cli.add.otus import add_otu
 from virtool_cli.options import debug_option, path_option
-from virtool_cli.ref.repo import EventSourcedRepo as Repo
 from virtool_cli.utils.logging import configure_logger
 
 
@@ -17,29 +16,47 @@ def add():
 
 @add.command()
 @click.argument(
+    "accession_",
+    metavar="ACCESSION",
+    type=str,
+)
+@path_option
+@debug_option
+def accession(accession_: str, debug: bool, path: Path):
+    """Retrieves data for the accession."""
+    configure_logger(debug)
+
+
+    try:
+        asyncio.run(add_accession(accession_, path))
+    except (FileNotFoundError, NotADirectoryError):
+        click.echo("Not a valid reference directory", err=True)
+
+
+@add.command()
+@click.argument(
     "accessions_",
     metavar="ACCESSIONS",
     nargs=-1,
     type=str,
 )
-@click.option("--taxid", type=int, required=True)
-@path_option
+@click.option(
+    "--otu-path",
+    type=click.Path(exists=True, file_okay=False, path_type=Path),
+)
 @debug_option
-def accessions(debug, path, taxid, accessions_: list[str]):
+def accessions(accessions_: list[str], otu_path, debug):
     """Fetch and write the data for the given NCBI accessions to an OTU.
 
-    virtool add accessions --taxid 2697049 MN996528.1 --path [repo_path]
+    virtool add accessions --otu-path ./tmv MN908947.3 MN996528.1
 
     """
     configure_logger(debug)
 
-    repo = Repo(path)
-
-    otu_index = repo.index_otus()
-    if taxid not in otu_index:
-        add_otu(repo, taxid)
-
-    add_accessions(repo, taxid, accessions_)
+    try:
+        asyncio.run(add_accessions(list(accessions_), otu_path))
+    except (FileNotFoundError, NotADirectoryError):
+        click.echo("could not find otu directory", err=True)
 
 
 @add.command()
@@ -52,3 +69,23 @@ def accessions(debug, path, taxid, accessions_: list[str]):
 def otu(debug: bool, path: Path, taxid: int):
     """Add a new OTU using a NCBI taxonomy ID."""
     configure_logger(debug)
+
+    try:
+        asyncio.run(add_otu(taxid, path))
+    except (FileNotFoundError, NotADirectoryError):
+        click.echo("Not a valid OTU directory", err=True)
+
+
+def split_clean_csv_string(input_string: str, delimiter: str = ",") -> list[str]:
+    """Splits comma-separated values into a list of strings.
+
+    :param input_string: A raw comma-delineated string
+    :param delimiter: A delimiter string for use with split(). Defaults to a comma (",")
+    :return: A list of accession strings
+    """
+    raw_list = input_string.split(delimiter)
+    stripped_list = []
+    for item in raw_list:
+        stripped_list.append(item.strip())
+
+    return stripped_list
