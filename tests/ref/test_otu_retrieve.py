81458064ea77385067930b820c86ddca8a72c3f9
Shelley Gao <syugao@gmail.com>
2024-06-05 14:51:28 -0700

    Equality tests complete

--- tests/ref/test_otu_retrieve.py
+++ tests/ref/test_otu_retrieve.py
@@ -2,30 +2,11 @@ from uuid import UUID
 
 import pytest
 
-from virtool_cli.ref.resources import EventSourcedRepoOTU, EventSourcedRepoSequence
-
-
-class TestOTU:
-    # @pytest.mark.skip()
-    @pytest.mark.parametrize("taxid", [345184])
-    def test_equivalence(self, taxid, scratch_repo):
-        otu = scratch_repo.get_otu_by_taxid(taxid)
-
-        assert type(otu) is EventSourcedRepoOTU
-
-        otu_copy = EventSourcedRepoOTU(
-            uuid=otu.id,
-            taxid=otu.taxid,
-            name=otu.name,
-            acronym=otu.acronym,
-            molecule=otu.molecule,
-            legacy_id=otu.legacy_id,
-            schema=otu.schema,
-            excluded_accessions=otu.excluded_accessions,
-            isolates=otu.isolates,
-        )
-
-        assert otu == otu_copy
+from virtool_cli.ref.resources import (
+    EventSourcedRepoOTU,
+    EventSourcedRepoIsolate,
+    EventSourcedRepoSequence,
+)
 
 
 class TestSequence:
@@ -49,3 +30,44 @@ class TestSequence:
             sequence_copy = EventSourcedRepoSequence(**sequence.dict())
 
             assert sequence == sequence_copy
+
+
+class TestIsolate:
+    @pytest.mark.parametrize("taxid", [345184])
+    def test_equivalence(self, taxid, scratch_repo):
+        otu = scratch_repo.get_otu_by_taxid(taxid)
+
+        for isolate in otu.isolates:
+            assert type(isolate) is EventSourcedRepoIsolate
+
+            isolate_copy = EventSourcedRepoIsolate(
+                uuid=isolate.id,
+                name=isolate.name,
+                legacy_id=isolate.legacy_id,
+                sequences=isolate.sequences,
+            )
+
+            assert isolate == isolate_copy
+
+
+class TestOTU:
+    # @pytest.mark.skip()
+    @pytest.mark.parametrize("taxid", [345184])
+    def test_equivalence(self, taxid, scratch_repo):
+        otu = scratch_repo.get_otu_by_taxid(taxid)
+
+        assert type(otu) is EventSourcedRepoOTU
+
+        otu_copy = EventSourcedRepoOTU(
+            uuid=otu.id,
+            taxid=otu.taxid,
+            name=otu.name,
+            acronym=otu.acronym,
+            molecule=otu.molecule,
+            legacy_id=otu.legacy_id,
+            schema=otu.schema,
+            excluded_accessions=otu.excluded_accessions,
+            isolates=otu.isolates,
+        )
+
+        assert otu == otu_copy
