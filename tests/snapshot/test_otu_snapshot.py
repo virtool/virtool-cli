48098bb3f394422c7df7277e3b9e0e8719b620d4
Shelley Gao <syugao@gmail.com>
2024-06-13 14:12:22 -0700

    New otu snapshot tests

--- tests/snapshot/test_otu_snapshot.py
+++ tests/snapshot/test_otu_snapshot.py
@@ -1,5 +1,6 @@
 from uuid import UUID
 
+import orjson
 import pytest
 
 from virtool_cli.ref.snapshot.otu import OTUSnapshot
@@ -10,18 +11,22 @@ def empty_otu_snapshot_path(tmp_path):
     return tmp_path / "empty_otu_snapshot"
 
 
+@pytest.mark.parametrize("taxid", [438782, 1441799, 430059])
 class TestOTUSnapshot:
-    @pytest.mark.parametrize("taxid", [438782, 1441799, 430059])
-    def test_cache(self, taxid: int, scratch_repo, empty_otu_snapshot_path):
+    def test_snapshot_direct(self, taxid: int, scratch_repo, empty_otu_snapshot_path):
         otu = scratch_repo.get_otu_by_taxid(taxid)
 
         otu_snapshotter = OTUSnapshot(empty_otu_snapshot_path)
 
         otu_snapshotter.cache(otu, options=None)
 
+        assert otu_snapshotter.at_event is None
+
         assert (otu_snapshotter.path / "otu.json").exists()
 
         assert (otu_snapshotter.path / "toc.json").exists()
+
+        assert (otu_snapshotter.path / "metadata.json").exists()
 
         data_dir = otu_snapshotter.path / "data"
 
@@ -32,3 +37,19 @@ class TestOTUSnapshot:
         isolate_ids = set(isolate.id for isolate in otu.isolates)
 
         assert isolate_ids.issubset(data_set)
+
+    def test_snapshot_from_index(self, taxid: int, scratch_repo):
+        scratch_repo.snapshot()
+
+        otu_id = scratch_repo.index_by_taxid[taxid]
+
+        otu_snapshot_path = scratch_repo.cache_path / f"snapshot/{otu_id}"
+
+        assert otu_snapshot_path.exists()
+
+        assert otu_snapshot_path / "metadata.json"
+
+        with open((otu_snapshot_path / "metadata.json"), "rb") as f:
+            metadata_dict = orjson.loads(f.read())
+
+        assert type(metadata_dict["at_event"]) is int
